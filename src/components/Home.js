8b0ce37016d5c725eb0c7ddfa529621374a908d5
Jay Kesner <jaykesner@gmail.com>
2021-11-16 12:58:27 -0500

    import mantine design kit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,66 @@
 import logo from "../directchat-logo.jpg";
+import svg1 from "../chat-svg1.svg";
+import svg2 from "../chat-svg2.svg";
+import svg3 from "../chat-svg3.svg";
+import { ReactComponent as EnterIcon } from "../enter-icon.svg";
 import { newChat, joinChat, randomChat } from "../firebase";
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
+import {
+  Container,
+  Title,
+  createStyles,
+  Group,
+  Text,
+  Button,
+  TextInput,
+  Center,
+  Image,
+  Grid,
+  Col,
+  AppShell,
+  Header,
+  Box,
+  SimpleGrid,
+  ThemeIcon,
+  UnstyledButton,
+} from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+const useStyles = createStyles((theme) => ({
+  landing: {
+    height: "80vh",
+  },
+  info: {
+    backgroundColor: theme.colors.gray[2],
+    height: "45vh",
+  },
+  info2: {
+    backgroundColor: theme.colors.gray[3],
+    height: "45vh",
+  },
+  button: {
+    "&:hover": {
+      backgroundColor: theme.colors.blue[5],
+    },
+  },
+  footer: {
+    backgroundColor: theme.colors.gray[6],
+    height: "45vh",
+  },
+  test1: {
+    backgroundColor: theme.colors.red[1],
+  },
+  test2: {
+    backgroundColor: theme.colors.red[2],
+  },
+}));
 
 export default function Home() {
   let history = useHistory();
-
   const [roomId, setRoomId] = useState("");
+  const { classes } = useStyles();
+  const matches = useMediaQuery("(min-width: 768px)");
 
   const addNewChat = async () => {
     const chatId = await newChat();
@@ -15,6 +69,19 @@ export default function Home() {
   };
 
   const joinChatById = async () => {
+    if (roomId) {
+      const chatRoom = await joinChat(roomId);
+      if (chatRoom) {
+        history.push(`/${roomId}`);
+      }
+      console.log(chatRoom);
+    } else {
+      return;
+    }
+  };
+
+  const joinChatById2 = async (event) => {
+    event.preventDefault();
     if (roomId) {
       const chatRoom = await joinChat(roomId);
       if (chatRoom) {
@@ -39,26 +106,66 @@ export default function Home() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div style={{ fontSize: "12px" }}>directchat.netlify.app</div>
-        <div>test room: XY1B2</div>
-        <input
-          type="text"
-          value={roomId}
-          onChange={(e) => setRoomId(e.target.value)}
-        />
-        <button className="Home-Button" onClick={() => joinChatById()}>
-          JOIN
-        </button>
-        <button className="Home-Button" onClick={() => joinRandomChat()}>
-          RANDOM
-        </button>
-        <button className="Home-Button" onClick={() => addNewChat()}>
-          NEW
-        </button>
-      </header>
-    </div>
+    <>
+      <Center className={classes.landing}>
+        <Group direction="column" position="center">
+          <Title>Direct Chat</Title>
+          <Text size="xs">directchat.netlify.app</Text>
+          <form onSubmit={(e) => joinChatById2(e)}>
+            <TextInput
+              placeholder="XY1B2"
+              label="Room ID"
+              value={roomId}
+              onChange={(event) => setRoomId(event.target.value)}
+              rightSection={
+                <UnstyledButton onClick={() => joinChatById()}>
+                  <ThemeIcon className={classes.button}>
+                    <EnterIcon />
+                  </ThemeIcon>
+                </UnstyledButton>
+              }
+            />
+          </form>
+          <Group>
+            <Button className={classes.button} onClick={() => addNewChat()}>
+              New
+            </Button>
+            <Button
+              className={classes.button}
+              onClick={() => joinRandomChat()}
+              variant="gradient"
+              gradient={{ from: "teal", to: "blue", deg: 60 }}
+            >
+              Random
+            </Button>
+          </Group>
+        </Group>
+      </Center>
+      <Center className={classes.info}>
+        <Group grow direction={matches ? "row" : "column"}>
+          <Image width={matches ? 400 : 300} alt="svg image" src={svg1} />
+          <Box>
+            <Title align="center" order={2} style={{ marginBottom: 10 }}>
+              Centered text title
+            </Title>
+            <Text align="center" color="primary">
+              Centered description text.
+            </Text>
+          </Box>
+        </Group>
+      </Center>
+      <Center className={classes.info2}>
+        <Group grow direction={matches ? "row" : "column-reverse"}>
+          <Box>
+            <Title align="center" order={2}>
+              Centered text title
+            </Title>
+            <Text align="center">Cenetered description text.</Text>
+          </Box>
+          <Image width={matches ? 400 : 300} alt="svg image" src={svg3} />
+        </Group>
+      </Center>
+      <Center className={classes.footer} />
+    </>
   );
 }
